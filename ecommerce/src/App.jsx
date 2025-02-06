af83c4043d921eb3e31a0f3e8de2889db6278fb1
xuanloi1999 <55650190+xuanloi1999@users.noreply.github.com>
2025-02-07 01:40:39 +0700

    add advance headling

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import '@styles/main.scss';
 import HomePage from '@components/HomePage/HomePage';
-import Info from '@components/Info/Info';
 
 function App() {
     return (
         <>
             <HomePage />
-            <Info />
         </>
     );
 }
--- ecommerce/src/components/HomePage/HomePage.jsx
+++ ecommerce/src/components/HomePage/HomePage.jsx
@@ -1,6 +1,8 @@
 import Banner from '@components/Banner/Banner';
 import Header from '@components/Header/Header';
 import styles from './styles.module.scss';
+import AdvanceHeadling from '@components/AdvanceHeadling/AdvanceHeadling';
+import Info from '@components/Info/Info';
 
 function HomePage() {
     const { container } = styles;
@@ -9,6 +11,8 @@ function HomePage() {
             <div className={container}>
                 <Header />
                 <Banner />
+                <Info />
+                <AdvanceHeadling />
             </div>
         </div>
     );
