36352b818e69ab4b87c328def87f146e03621f56
xuanloi1999 <55650190+xuanloi1999@users.noreply.github.com>
2025-02-07 23:11:18 +0700

    add classNames package for concat class

--- ecommerce/src/components/Button/Button.jsx
+++ ecommerce/src/components/Button/Button.jsx
@@ -1,8 +1,18 @@
+import classNames from 'classnames';
 import styles from './styles.module.scss';
 
-function Button({ content }) {
-    const { btn } = styles;
-    return <button className={btn}>{content}</button>;
+function Button({ content, isPrimary = true }) {
+    const { btn, primaryBtn, secondaryBtn } = styles;
+    return (
+        <button
+            className={classNames(btn, {
+                [primaryBtn]: isPrimary,
+                [secondaryBtn]: !isPrimary,
+            })}
+        >
+            {content}
+        </button>
+    );
 }
 
 export default Button;
