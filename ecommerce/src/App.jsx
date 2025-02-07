ee2c72fe767f678b0e40373fcd955584c0680a87
xuanloi1999 <55650190+xuanloi1999@users.noreply.github.com>
2025-02-07 23:58:40 +0700

    add routers

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import '@styles/main.scss';
 import HomePage from '@components/HomePage/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import routers from '@/routers/routers';
 
 function App() {
+    console.log('routers:', routers);
+
     return (
-        <>
-            <HomePage />
-        </>
+        <BrowserRouter>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    {routers.map(({ path, component: Component }, index) => (
+                        <Route
+                            key={index}
+                            path={path}
+                            element={<Component />}
+                        />
+                    ))}
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
     );
 }
 
